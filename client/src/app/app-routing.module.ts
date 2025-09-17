d19d33a13d9c7033cbb515b9978a7b19f37d3700
Quang Duy <87593754+quaqduy@users.noreply.github.com>
2025-09-18 03:17:18 +0700

    navbar v1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,42 @@
-import { AuthComponent } from './pages/settings/auth/auth.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/catalog/products/products.component';
+import { CategoriesComponent } from './pages/catalog/categories/categories.component';
+import { BrandsComponent } from './pages/catalog/brands/brands.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { LoyaltyComponent } from './pages/loyalty/loyalty.component';
+import { DiscountComponent } from './pages/discount/discount.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/admin/users/users.component';
+import { AuthComponent } from './pages/account/auth/auth.component';
+import { ProfileComponent } from './pages/account/profile/profile.component';
+import { DiscountsComponent } from './pages/admin/discounts/discounts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'dash', component: DashboardComponent },
-  { path: 'auth/login', component: AuthComponent },
-  { path: 'auth/logout', component: DashboardComponent },
+
+  { path: 'home', component: HomeComponent },
+
+  { path: 'catalog/products', component: ProductsComponent },
+  { path: 'catalog/categories', component: CategoriesComponent },
+  { path: 'catalog/brands', component: BrandsComponent },
+
+  { path: 'cart', component: CartComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'loyalty', component: LoyaltyComponent },
+  { path: 'discount', component: DiscountComponent },
+
+  { path: 'account/auth', component: AuthComponent },
+  { path: 'account/profile', component: ProfileComponent },
+
+  { path: 'admin/dashboard', component: DashboardComponent },
+  { path: 'admin/users', component: UsersComponent },
+  { path: 'admin/products', component: ProductsComponent },
+  { path: 'admin/orders', component: OrdersComponent },
+  { path: 'admin/discounts', component: DiscountsComponent },
+
   { path: '**', redirectTo: '/home' },
 ];
 
---
+++ client/src/app/pages/admin/discounts/discounts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discounts',
+  templateUrl: './discounts.component.html',
+  styleUrls: ['./discounts.component.scss']
+})
+export class DiscountsComponent {
+
+}
