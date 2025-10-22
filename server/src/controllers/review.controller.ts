1688a77a307707a71596cd11666d69e119f9b51d
hoangkiendev <kien@Kiens-MacBook-Pro.local>
2025-10-23 00:38:26 +0700

    feat(comment): add comment API, allow user comment without log in

--- server/src/controllers/review.controller.ts
+++ server/src/controllers/review.controller.ts
@@ -2,12 +2,32 @@ import { Request, Response } from "express";
 import { NotFoundError } from "../core/error.response";
 import ReviewService from "../services/review.service";
 import { OK } from "../core/success.response";
+import { HashIP } from "../utils/crypto.handler";
+import { Types } from "mongoose";
 
 
 
 class ReviewController {
     static async AddComment(req: Request, res: Response): Promise<void> {
+        const { productId } = req.params;
+        const userId = req.user?.userId;
+        const { content, displayName } = req.body;
+        const hashedIP = HashIP(req.ip);
 
+        if (!productId) throw new NotFoundError("Missing productId");
+
+        const response = await ReviewService.AddComment({
+            productId: Types.ObjectId.isValid(productId) ? new Types.ObjectId(productId) : productId,
+            displayName: displayName,
+            content: content,
+            userId: userId ? (Types.ObjectId.isValid(userId) ? new Types.ObjectId(userId) : null) : null,
+            ipHash: hashedIP || ""
+        });
+
+        new OK({
+            message: "Thêm comment thành công",
+            data: { comment: response }
+        }).send(res);
     }
 
     static async ListComments(req: Request, res: Response): Promise<void> {
@@ -15,10 +35,9 @@ class ReviewController {
         const page = Math.max(1, Number(req.query.page) || 1);
         const limit = Math.min(50, Number(req.query.limit) || 10);
 
-        if(!productId) throw new NotFoundError("Missing credentials");
+        if (!productId) throw new NotFoundError("Missing credentials");
 
-        const response = await ReviewService.ListComments({productId, page, limit});
-        console.log('Response: ', response);
+        const response = await ReviewService.ListComments({ productId, page, limit });
 
         new OK({
             message: "Lấy danh sách comment thành công",
@@ -38,6 +57,19 @@ class ReviewController {
         const { productId } = req.params;
         const page = Math.max(1, Number(req.query.page) || 1);
         const limit = Math.min(50, Number(req.query.limit) || 10);
+
+        if (!productId) throw new NotFoundError("Missing credentials");
+
+        const response = await ReviewService.ListRatings({ productId, page, limit });
+
+        new OK({
+            message: "Lấy danh sách rating thành công",
+            data: {
+                ratings: response.ratings,
+                total: response.total,
+                page: page
+            }
+        }).send(res);
     }
 }
 
--- server/src/repositories/review.repository.ts
+++ server/src/repositories/review.repository.ts
@@ -1,6 +1,7 @@
 
-import { IComment } from "../interfaces/review.interface";
+import { IComment, IRating } from "../interfaces/review.interface";
 import Comment from "../models/comment.model";
+import Rating from "../models/rating.model";
 
 
 class ReviewRepo {
@@ -36,6 +37,35 @@ class ReviewRepo {
     }
 
     // ==============RATING===============
+    static async createRating(data: Partial<IRating>) {
+        const rating = new Rating(data);
+        return await rating.save();
+    }
+
+    static async findRatingById(id: string) {
+        return await Rating.findById(id).lean();
+    }
+
+    static async deleteRatingById(id: string) {
+        return await Rating.findByIdAndDelete(id);
+    }
+
+    static async deleteManyRating(ids: string[]) {
+        return await Rating.deleteMany({ _id: { $in: ids } });
+    }
+
+    static async updateRating(commentId: string, commentData: Partial<IComment>) {
+        return Rating.findByIdAndUpdate(commentId, commentData, { new: true }).exec();
+    }
+
+    static async findAllRatings(productId: string, skip = 0, limit = 10) {
+        const [ratings, total] = await Promise.all([
+            Rating.find({ productId }).skip(skip).limit(limit).sort({ createdAt: -1 }).lean(),
+            Rating.countDocuments({ productId })
+        ]);
+
+        return { ratings, total };
+    }
 }
 
 export default ReviewRepo;
