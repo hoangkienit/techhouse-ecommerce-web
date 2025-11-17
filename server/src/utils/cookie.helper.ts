d53693965c3f6f570372d3f738328d970f29ab44
hoangkienit <kiennguyen4321abc2gmail.com>
2025-11-17 13:09:41 +0700

    feat(cookie): update SameSite attribute for cookies to 'none'

--- server/src/utils/cookie.helper.ts
+++ server/src/utils/cookie.helper.ts
@@ -5,14 +5,14 @@ const saveTokenToCookie = (accessToken: string, refreshToken: string, res: Respo
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 30 * 60 * 1000
     });
 
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
 }
